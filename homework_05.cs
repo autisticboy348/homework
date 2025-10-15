0fddac665def6ab656b654aad13d3ae2e816dbb1
autisticboy348 <gospodnikov9roman@gmail.com>
2025-10-15 19:13:37 +0300

    Add files via upload

---
+++ homework_05.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("write sides of triangle:");
+
+int side1 = Convert.ToInt32(Console.ReadLine());
+int side2 = Convert.ToInt32(Console.ReadLine());
+int side3 = Convert.ToInt32(Console.ReadLine());
+
+if (side1 == side2 && side2 == side3)
+{
+    Console.WriteLine("equilateral");
+}
+else if (side1 == side2 || side2 == side3)
+{
+    Console.WriteLine("isosceles");
+}
+else if (side1 != side2 && side1 != side3 || side2 != side3)
+{
+    Console.WriteLine("versatile");
+}
+else
+{
+    Console.WriteLine("error!");
+}
