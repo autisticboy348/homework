f3d1004c00c07e1b8c8969d350d95328d118da46
autisticboy348 <gospodnikov9roman@gmail.com>
2025-10-16 18:38:01 +0300

    Add files via upload

---
+++ homework2.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("--- Калькулятор Индекса Массы Тела (ИМТ) ---");
+Console.WriteLine(" ");
+Console.WriteLine("Введите ваш вес в килограммах:");
+int weight = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите ваш рост в метрах (например, 1.75):");
+
+Console.WriteLine(" ");
+Console.WriteLine("--- Ваш результат ---");
+double height = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"При весе {weight} кг и росте {height} м, ваш ИМТ составляет: {weight / (height * height)}");
