96d98e6e05fd20fa05aeca0c4ba3a08d3d114ec2
autisticboy348 <gospodnikov9roman@gmail.com>
2025-10-22 19:51:43 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("your number here: ");
+        int n = int.Parse(Console.ReadLine());
+
+        if (n < 0)
+        {
+            Console.WriteLine("error");
+            return;
+        }
+
+        int result = CalculateFactorial(n);
+        Console.WriteLine($"factorial of number {n} is {result}");
+    }
+
+    static int CalculateFactorial(int n)
+    {
+        int factorial = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            factorial *= i;
+        }
+
+        return factorial;
+    }
+}
