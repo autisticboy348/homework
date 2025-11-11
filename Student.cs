776f85fbbc5c415113163ef6474f58b96a072283
autisticboy348 <gospodnikov9roman@gmail.com>
2025-11-11 14:16:45 +0300

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,38 @@
+﻿public class Student
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    private int _age;
+    public int Age
+    {
+        get { return _age; }
+        set
+        {
+            if (value >= 6 && value <= 100)
+            {
+                _age = value;
+            }
+            else
+            {
+                Console.WriteLine($"Ошибка: возраст {value} некорректен. Возраст должен быть от 6 до 100 лет.");
+            }
+        }
+    }
+
+    public string FullName
+    {
+        get { return $"{FirstName} {LastName}"; }
+    }
+
+    public Student(string firstName, string lastName, int age)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Age = age;
+    }
+
+    public Student() : this("", "", 0)
+    {
+    }
+}
